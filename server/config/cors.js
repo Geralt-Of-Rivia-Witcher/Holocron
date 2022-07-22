a418368503de638112fe952597c2840372108bb7
Geralt-Of-Rivia-Witcher <singhsiddhantkumar@gmail.com>
2022-07-23 04:07:47 +0530

    update: cors whitelist

--- server/config/cors.js
+++ server/config/cors.js
@@ -1,5 +1,16 @@
+var whitelist = [
+    "http://localhost:3000",
+    "https://holocron.siddhantkumarsingh.me",
+];
+
 export const corsOptions = {
-    origin: ["http://localhost:3000", "https://holocron.siddhantkumarsingh.me"],
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true,
     exposedHeaders: ["set-cookie"],
 };
