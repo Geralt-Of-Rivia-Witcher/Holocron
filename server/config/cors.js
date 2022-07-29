929ca0df8d4777fa53a44eefcdc2fb0f463d1bce
Geralt-Of-Rivia-Witcher <singhsiddhantkumar@gmail.com>
2022-07-29 17:29:04 +0530

    CORS Preflight

--- server/config/cors.js
+++ server/config/cors.js
@@ -10,7 +10,9 @@ const allowCors = (fn) => async (req, res) => {
         "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
     );
     if (req.method === "OPTIONS") {
-        return res.status(200).end();
+        return res.status(200).json({
+            body: "OK",
+        });
     }
     return await fn(req, res);
 };
